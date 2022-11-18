d07093a3139a7539285eacb733a1c52d671e6e06
Xavier Brigode <xavier.brigode@gmail.com>
2022-11-18 16:22:15 +0100

    add style to all project, darkmode behavior, ...

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,27 +1,29 @@
 import { fetchCountries } from "./fetchCountries.js";
 import { fetchCountry } from "./fetchCountry.js";
-const modal = document.querySelector(".modal")
+import { toggleDarkMode } from "./darkMode.js";
+const modal = document.querySelector(".modal");
+const darkMode = document.querySelector(".darkMode");
+const darkModeDetail = document.querySelector(".darkModeDetail");
+
+darkMode.addEventListener("click", toggleDarkMode);
 
 async function openCountryModal(event) {
     let country = event.currentTarget.dataset.country;
     let countryData = await fetchCountry(country); // FETCH ONE CONTRY SELECTED
     modal.showModal();
-    const currencyArray = Object.values(countryData[0].currencies)
-    const languageArray = Object.values(countryData[0].languages)
+    const currencyArray = Object.values(countryData[0].currencies);
+    const languageArray = Object.values(countryData[0].languages);
     function getBorder() {
-
         let borderCountries = [];
         for (const element of countryData[0].borders) {
-            borderCountries += `<a>${element}</a>`
+            borderCountries += `<a>${element}</a>`;
         }
-        return borderCountries
-
-
+        return borderCountries;
     }
     let htmlSection = `
         <section class="modal__header">
             <h2>Where in the world</h2>
-            <div class="darkMode">
+            <div class="darkModeDetail">
                 <img
                     class="darkMode__icon"
                     src="./assets/img/moon-outline.svg"
@@ -36,19 +38,25 @@ async function openCountryModal(event) {
         </div>
         <div class="modal__container">
             <div class="modal__container__flag">
-                <img src="${countryData[0].flags.png}" alt="${countryData[0].name.common}">
+                <img src="${countryData[0].flags.png}" alt="${
+        countryData[0].name.common
+    }">
             </div>
             <div class="modal__container__information">
                 <h2>${countryData[0].name.common}</h2>
                 <div class="modal__container__information__description">
-                    <span>Native Name: ${countryData[0].name.official}</span>
-                    <span>Population: ${countryData[0].population}</span>
-                    <span>Region: ${countryData[0].region}</span>
-                    <span>Sub Region: ${countryData[0].subregion}</span>
-                    <span>Capital: ${countryData[0].capital}</span>
-                    <span><br>Top Level Domain: ${countryData[0].tld[0]}</span>
-                    <span>Currencies: ${currencyArray[0].name}</span>
-                    <span>Languages: ${languageArray[0]}</span>
+                    <span><b>Native Name:</b> ${
+                        countryData[0].name.official
+                    }</span>
+                    <span><b>Population:</b> ${countryData[0].population}</span>
+                    <span><b>Region:</b> ${countryData[0].region}</span>
+                    <span><b>Sub Region:</b> ${countryData[0].subregion}</span>
+                    <span><b>Capital:</b> ${countryData[0].capital}</span>
+                    <span><br><b>Top Level Domain:</b> ${
+                        countryData[0].tld[0]
+                    }</span>
+                    <span><b>Currencies:</b> ${currencyArray[0].name}</span>
+                    <span><b>Languages:</b> ${languageArray[0]}</span>
                 </div>
                 <div class="modal__container__information__border">
                     <p>Border Countries: </p>
@@ -57,14 +65,14 @@ async function openCountryModal(event) {
                     </div>
                 </div>
             </div>
-        </div>`
+        </div>`;
 
 
-    modal.innerHTML = htmlSection
-    const closeModal = document.querySelector(".close-button")
-    closeModal.addEventListener('click', function () {
+    modal.innerHTML = htmlSection;
+    const closeModal = document.querySelector(".close-button");
+    closeModal.addEventListener("click", function () {
         modal.close();
-    })
+    });
 }
 
 fetchCountries()
@@ -76,20 +84,23 @@ fetchCountries()
             let country = countries[randomNum]; // Random pick country
             let htmlSegment = `<div class='country' data-country=${country.name.common} id='${i}'> 
                                 <img class='country__flag' src="${country.flags.png}" alt="">
-                                <h2 class='country__name'>${country.name.common}</h2>
-                                <span class='country__population'>Population: ${country.population}</span>
-                                <span class='country__region'>Region: ${country.region}</span>
-                                <span class='country__capital'>Capital: ${country.capital} </span>
+                                <div class='country__description'>
+                                    <h2 class='country__name'>${country.name.common}</h2>
+                                    <span class='country__population'><b>Population:</b> ${country.population}</span>
+                                    <span class='country__region'><b>Region:</b> ${country.region}</span>
+                                    <span class='country__capital'><b>Capital:</b> ${country.capital} </span>
+                                </div>
+
                             </div>`; // country card html
             html += htmlSegment;
         }
         container.innerHTML = html;
     })
     .then(() => {
-        let countries = document.getElementsByClassName("country")
+        let countries = document.getElementsByClassName("country");
         for (let country of countries) {
-            country.classList.add('open-button')
-            country.classList.add('button')
-            country.addEventListener("click", openCountryModal)
+            country.classList.add("open-button");
+            country.classList.add("button");
+            country.addEventListener("click", openCountryModal);
         }
-    })
+    });
---
+++ assets/scripts/darkMode.js
@@ -0,0 +1,29 @@
+export function toggleDarkMode() {
+    const icon = document.querySelector(".darkMode__icon");
+    const search = document.querySelector(".search-container__search");
+    const header = document.querySelector("header");
+    const searchIcon = document.getElementById("searchIcon");
+    const searchText = document.getElementById("searchText");
+    const filter = document.querySelector(".search-container__filter")
+
+    document.body.classList.toggle("darkModeAll");
+    document.querySelector(".modal").classList.toggle('darkModeAll')
+    if (document.body.classList.contains("darkModeAll")) {
+        icon.src = "./assets/img/sunny-outline.svg";
+        header.style.backgroundColor = "#2b3945";
+        search.style.backgroundColor = "#2b3945";
+        searchIcon.style.backgroundColor = "#2b3945";
+        searchText.style.backgroundColor = "#2b3945";
+        filter.style.backgroundColor = "#2b3945";
+        filter.style.color = "#858585";
+    } else {
+        icon.src = "./assets/img/moon-outline.svg";
+        header.style.backgroundColor = "#fafafa";
+        search.style.backgroundColor = "#fafafa";
+        searchIcon.style.backgroundColor = "#fafafa";
+        searchText.style.backgroundColor = "#fafafa";
+        filter.style.backgroundColor = "#fafafa";
+        filter.style.color = "#858585";
+
+    }
+}
