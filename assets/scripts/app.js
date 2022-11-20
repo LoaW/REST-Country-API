10ce4f0b9437c807ed4dbe521ea5cc02f29e0349
Xavier Brigode <xavier.brigode@gmail.com>
2022-11-20 19:35:40 +0100

    search by inputfield

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,84 @@
 import { fetchCountries } from "./fetchCountries.js";
 import { fetchCountry } from "./fetchCountry.js";
 import { toggleDarkMode } from "./darkMode.js";
+import { autocomplete } from "./autoComplete.js";
 const modal = document.querySelector(".modal");
 const darkMode = document.querySelector(".darkMode");
+const inputBtn = document.getElementById("searchIcon")
 
 darkMode.addEventListener("click", toggleDarkMode);
+inputBtn.addEventListener("click", openInputCountry)
+
+async function openInputCountry(){
+    let inputCountry = document.getElementById("searchText").value
+    console.log(inputCountry)
+    let countryData = await fetchCountry(inputCountry); // FETCH ONE CONTRY SELECTED
+    modal.showModal();
+    const currencyArray = Object.values(countryData[0].currencies);
+    const languageArray = Object.values(countryData[0].languages);
+    function getBorder() {
+        let borderCountries = [];
+        for (const element of countryData[0].borders) {
+            borderCountries += `<a>${element}</a>`;
+        }
+        return borderCountries;
+    }
+    let htmlSection = `
+        <section class="modal__header">
+            <h2>Where in the world</h2>
+            <div class="darkModeDetail">
+                <img
+                    class="darkModeModal__icon"
+                    src="./assets/img/moon-outline.svg"
+                    alt="darkMode__icon"
+                />
+                <span class="darkMode__text">Dark Mode</span>
+            </div>
+        </section>
+        <div class="button close-button">
+            <img src="./assets/img/retour.png" alt="">
+            <span>Back</span>
+        </div>
+        <div class="modal__container">
+            <div class="modal__container__flag">
+                <img src="${countryData[0].flags.png}" alt="${
+        countryData[0].name.common
+    }">
+            </div>
+            <div class="modal__container__information">
+                <h2>${countryData[0].name.common}</h2>
+                <div class="modal__container__information__description">
+                    <span><b>Native Name:</b> ${
+                        countryData[0].name.official
+                    }</span>
+                    <span><b>Population:</b> ${countryData[0].population}</span>
+                    <span><b>Region:</b> ${countryData[0].region}</span>
+                    <span><b>Sub Region:</b> ${countryData[0].subregion}</span>
+                    <span><b>Capital:</b> ${countryData[0].capital}</span>
+                    <span><br><b>Top Level Domain:</b> ${
+                        countryData[0].tld[0]
+                    }</span>
+                    <span><b>Currencies:</b> ${currencyArray[0].name}</span>
+                    <span><b>Languages:</b> ${languageArray[0]}</span>
+                </div>
+                <div class="modal__container__information__border">
+                    <p>Border Countries: </p>
+                    <div class="modal__container__information__border__country">
+                        ${getBorder()}
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    
+    modal.innerHTML = htmlSection;
+
+    let darkModeDetail = document.querySelector(".darkModeDetail");
+    darkModeDetail.addEventListener('click', toggleDarkMode)
+    const closeModal = document.querySelector(".close-button");
+    closeModal.addEventListener("click", function () {
+        modal.close();
+    });
+}
 
 async function openCountryModal(event) {
     let country = event.currentTarget.dataset.country;
@@ -80,6 +154,7 @@ fetchCountries()
     .then((countries) => {
         let html = "";
         let container = document.querySelector(".api-container"); // DOM container
+
         for (let i = 0; i < 8; i++) {
             let randomNum = Math.floor(Math.random() * countries.length); // Random number between 0 & 250
             let country = countries[randomNum]; // Random pick country
@@ -105,3 +180,4 @@ fetchCountries()
             country.addEventListener("click", openCountryModal);
         }
     });
+    
